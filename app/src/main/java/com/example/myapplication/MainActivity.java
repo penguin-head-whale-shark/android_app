9c5de81ab778765b18659ba228ea48cadaad14a1
yoone1123 <81676485+yoone1123@users.noreply.github.com>
2021-11-03 15:54:34 +0900

    안드로이드 앱

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,5 +18,8 @@ public class MainActivity extends AppCompatActivity {
     public void onButton1Clicked(View v) {
         Toast.makeText(this, "확인 1 버튼이 눌렸어요", Toast.LENGTH_LONG).show();
     }
-
+    public void onButton2Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(myIntent);
+    }
 }
