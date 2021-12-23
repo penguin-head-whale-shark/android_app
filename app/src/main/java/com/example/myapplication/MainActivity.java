1bfc68fa1a531f89deebadf1bb04ec01f1dd1916
yoone1123 <81676485+yoone1123@users.noreply.github.com>
2021-12-23 09:38:39 +0900

    Login Page 만들기 연습

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,14 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         System.out.println("실행 성공");
-    }
-    public void onButton1Clicked(View v){//context 는 환경정보를 담음
-        Toast.makeText(this,"버튼이 눌렸어요",Toast.LENGTH_LONG).show();
-        System.out.println("실행 완료");
-    }
-    public void onButton3Clicked(View v){
-        Toast.makeText(this,"검색이 눌렸어요",Toast.LENGTH_LONG).show();
     }
 }
