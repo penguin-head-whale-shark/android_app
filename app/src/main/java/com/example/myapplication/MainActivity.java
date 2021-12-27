1f0f863ce8f9db04220c0dbe31de3c34736f89bb
kdh1123 <kdh200533@gmail.com>
2021-12-27 10:04:33 +0900

    로그인 Xml 수정

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,22 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextInputEditText_id = findViewById(R.id.TextInputEditText_id);
-        TextInputEditText_password = findViewById(R.id.TextInputEditText_password);
-        Login_Button = findViewById(R.id.Login_Button);//변수에 뷰에서 찾아온 컴포넌트와 연결
 
-        //값을 가져온다 -> 클릭을 감지한다 ->  가져온 값을 다른 액티비티로 넘김
-        Login_Button.setClickable(true);//이 컴포넌트는 클릭을 가능하다고 설정
-        Login_Button.setOnClickListener(new View.OnClickListener() { //새로운 리스너 달아줌
-            @Override
-            public void onClick(View v){
-                String id = TextInputEditText_id.getText().toString();//입력한 값을 문자열로 반환
-                String password = TextInputEditText_password.getText().toString();//입력한 값을 문자열로 반환
-                Intent intent = new Intent(MainActivity.this, LoginResultActivity.class);
-                intent.putExtra("id",id);
-                intent.putExtra("password",password);
-                startActivity(intent);
-            }
-        });
     }
 }
