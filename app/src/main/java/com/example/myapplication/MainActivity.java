b1d89cf6bc502da142ae2c2630b44b2582c6ebe7
황지민 <80872558+krapoi@users.noreply.github.com>
2021-12-27 17:57:43 +0900

    Add Writing

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,13 +6,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.test);
         System.out.println("실행 성공");
+
+    }
+
+    public void monClick(View v){
+        Intent intent = new Intent(this,WriteActivity.class);
+        startActivity(intent);
     }
 }
